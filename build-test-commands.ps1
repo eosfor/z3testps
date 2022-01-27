0050552de9e116c69ec365dc862ba2dc6936f263
eosfor <eosfor@xxx.com>
2022-01-27 00:41:51 -0800

    + comments to show what to do to buikd and test

--- build-test-commands.ps1
+++ build-test-commands.ps1
@@ -1,19 +1,22 @@
-# cd "C:\Users\Andrey_Vernigora\source\repos\z3testps\z3testps\bin\Debug\net6.0"
-
-## buikd
+## run dotnet build in the project folder
 cd (join-path $env:HOMEDRIVE $env:HOMEPATH "source\repos\z3testps\z3testps")
 dotnet build && dotnet publish --self-contained --use-current-runtime -c Debug -o c:\temp\z3testps\
 
-
 ## test
 cd "c:\temp\z3testps\"
+Import-Module .\z3testps.dll -Verbose
 $pid | clip
 
-Import-Module .\z3testps.dll -Verbose
+
 $sourceVMs = Import-Csv .\vmdata.csv
 $sourceVMs  | % { $_.cpu = [int]$_.cpu; $_.ram = [int]$_.ram; $_.datadisk = [int]$_.datadisk; }
 
 $targetSizes = import-csv ".\vmCostACUData.csv"
 
 
-Start-Z3ModelCalculation -SourceVM $sourceVMs -TargetVM $targetSizes
+## run with only one source VM
+$m = Start-Z3ModelCalculation -SourceVM $sourceVMs[0] -TargetVM $targetSizes
+$m.Consts | % {$_.Tostring()}
+
+## run with all source VMs
+$m = Start-Z3ModelCalculation -SourceVM $sourceVMs -TargetVM $targetSizes
